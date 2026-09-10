d389ce99040b1bfc6f1a99a4b4457194ef89fca3
saurabh shendurkar <saurabhshendurkar3103@gmail.com>
2026-09-10 11:02:23 +0530

    create basic server

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+export function createApp(){
+    const app = express()
+
+    //middleware
+
+    //routes
+
+
+    return app
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello from init")
+import "dotenv/config"
+import { createServer } from "node:http"
+import { createApp } from "./app/app"
+
+const PORT=process.env.PORT||5000
+
+
+
+const start = async ()=>{
+    const server=createServer(createApp())
+
+    server.listen(PORT,()=>{
+        console.log(`server started on ${PORT}`);
+    })
+}
+
+start().catch(err=>{
+    console.log(`server failed to start due to : ${err}`)
+    process.exit(1)
+})
